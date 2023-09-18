2c76d8eb2f43d8e53fa1b396a136e185c7435273
Adam <adamsoliman800@gmail.com>
2023-09-18 20:35:20 +0300

    update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Parse JSON and form data
+// Parse JSON and form dataa
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
