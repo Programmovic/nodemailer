78e24d9010c175ca1c591707bc3cd8a77daa316c
Adam <adamsoliman800@gmail.com>
2023-09-18 20:51:20 +0300

    build(deps): update .package-lock.json, package-lock.json, package.json and server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+// Enable CORS for all routes
+app.use(cors());
 // Parse JSON and form dataa
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
